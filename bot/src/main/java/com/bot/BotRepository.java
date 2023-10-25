8b969899bd05b59c87b143822fd57e2f0b56be0b
Pavan <pavan.ingale@ijonaservices.com>
2023-10-25 12:20:04 +0530

    Correction

--- bot/src/main/java/com/bot/BotRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bot;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface BotRepository extends JpaRepository<Bot, Long> {
-    Optional<Bot> findByQuestion(String question);
-}
